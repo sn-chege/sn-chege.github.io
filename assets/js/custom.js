7d83acbb750915594b93450541f664fedc3ed22b
chege99 <leeblittz@gmail.com>
2020-06-16 18:20:36 +0300

    OCHEEZY SITE COMMIT

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    freeMode: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
